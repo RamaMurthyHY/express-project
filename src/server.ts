2d2739a3a47dc3b1c854fef96f7bd4c7532e4100
IamRamHY <reach.rambymail@gmail.com>
2023-06-02 14:29:57 +0530

    Introduce typescript

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import express from "express";
+import express, { Express, Request, Response } from "express";
 import { config } from "dotenv";
 config(); // Config function will map the variables defined in .env file to node process
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app: Express = express();
+const port: Number = Number(process.env.PORT) || 5000;
 
 app.use(express.json()); // This statement will parses the data stream to json which will comes from client;
 
-app.get("/health-check", (req, res) => {
+app.get("/health-check", (req: Request, res: Response) => {
   res.status(200).json({ message: "I am alive..!" });
 });
 
