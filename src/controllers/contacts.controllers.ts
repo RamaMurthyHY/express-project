39153bf426e5a4a3763a119d8832d6cf072afbcf
IamRamHY <reach.rambymail@gmail.com>
2023-06-06 15:45:14 +0530

    Add create contact api

--- src/controllers/contacts.controllers.ts
+++ src/controllers/contacts.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { Contacts } from "../db/models";
+import { StatusCodes } from "../enums";
 
 const getAllContacts = async (
   req: Request,
@@ -18,8 +19,34 @@ const getContact = (req: Request, res: Response) => {
   res.status(200).json({ message: "Get a contact" });
 };
 
-const createContact = (req: Request, res: Response) => {
-  res.status(201).json({ message: "Create contact" });
+const createContact = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, phone } = req.body || {};
+
+    if (!name || !email || !phone) {
+      res.status(StatusCodes.BadRequest);
+      throw new Error("All fields are mandatory");
+    }
+
+    const foundContact = await Contacts.findOne({ email, phone });
+    if (foundContact) {
+      res.status(StatusCodes.Conflict);
+      throw new Error("Contact already exist");
+    }
+
+    const contact = await Contacts.create({
+      name,
+      email,
+      phone,
+    });
+    res.status(201).json(contact);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const updateContact = (req: Request, res: Response) => {
