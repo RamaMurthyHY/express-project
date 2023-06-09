05d617346eea18d15eefa7aca40d144f4606116c
IamRamHY <reach.rambymail@gmail.com>
2023-06-09 14:31:11 +0530

    Make all contacts routes protected

--- src/routes/contacts.routes.ts
+++ src/routes/contacts.routes.ts
@@ -1,4 +1,5 @@
 import express, { Router } from "express";
+import { auth } from "../helpers";
 const router: Router = express.Router();
 
 import {
@@ -8,6 +9,8 @@ import {
   updateContact,
   deleteContact,
 } from "../controllers";
+
+router.use(auth.verifyAccessToken); // This statement ensure all routes are protected
 
 router.route("/").get(getAllContacts).post(createContact);
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
